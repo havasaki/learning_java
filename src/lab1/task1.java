39670b311a37b6abc602c2b95b83dbfe07aece28
havasaki <elcov.danilka@mail.ru>
2024-09-15 19:15:10 +0300

    create 1st task in lab1

--- src/lab1/task1.java
+++ src/lab1/task1.java
@@ -1,7 +1,14 @@
 package lab1;
 
 public class task1 {
-    public static void main(String[] args){
-        System.out.println(72);
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int sum = 0;
+        for (int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        double avg = sum/arr.length;
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Среднее арифметическое элементов массива: " + avg);
     }
 }
--- src/lab2/task1.java
+++
@@ -1,5 +0,0 @@
-package lab2;
-
-public class task1 {
-    world;
-}
