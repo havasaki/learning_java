226f63b6c2ecbd4cba3e1b46b51543c4c7be6197
havasaki <elcov.danilka@mail.ru>
2024-09-15 19:44:02 +0300

    create 3rd task in lab1

---
+++ src/lab1/task3.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class task3 {
+    public static void main (String[] args){
+        int n=10;
+        for(int i=1;i<=n;i++){
+            double row=1.0/i;
+            System.out.printf("%.4f\n",row);
+        }
+    }
+}
