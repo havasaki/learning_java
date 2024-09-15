73a9f906ca718290d31b6dd8cb8af5248e51c31e
havasaki <elcov.danilka@mail.ru>
2024-09-15 19:31:02 +0300

    create 2nd task in lab1

---
+++ src/lab1/task2.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class task2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Аргумент " + i + ": " + args[i]);
+        }
+    }
+}
