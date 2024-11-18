733cdf03a9992b6248e75647464068eee0607051
havasaki <elcov.danilka@mail.ru>
2024-11-18 21:08:00 +0300

    create 1 task in lab13

--- src/lab13/task1.java
+++ src/lab13/task1.java
@@ -1,42 +1,34 @@
 package lab13;
 
 public class task1 {
+    // Task 1
+    public static void printSomeString(String str){
+        System.out.println(str);
+    }
+
     public static void main(String[] args) {
-        String input = "I like Java!!!";
+        String str1 = "I like Java!!!";
+        // Task 1
+        printSomeString("Task 1:\n" + str1);
+        // Task 2
+        System.out.println("Task 2:\n" + str1.charAt(str1.length() - 1));
+        // Task 3
+        System.out.println("Task 3:\n" + str1.endsWith("!!!"));
+        // Task 4
+        System.out.println("Task 4:\n" + str1.startsWith("I like"));
+        // Task 5
+        System.out.println("Task 5:\n" + str1.contains("Java"));
+        // Task 6
+        System.out.println("Task 6:\n" + str1.indexOf("Java"));
+        // Task 7
+        System.out.println("Task 7:\n" + str1.replace('a', 'o'));
+        // Task 8
+        System.out.println("Task 8:\n" + str1.toUpperCase());
+        // Task 9
+        System.out.println("Task 9:\n" + str1.toLowerCase());
+        // Task 10
+        System.out.println("Task 10:\n" + str1.substring(0, str1.indexOf("Java"))
+                + str1.substring(str1.indexOf("Java") + "Java".length()));
 
-        // 2. Распечатать последний символ строки
-        System.out.println("Последний символ: " + input.charAt(input.length() - 1));
-
-        // 3. Проверить, заканчивается ли строка подстрокой "!!!"
-        boolean endsWithExclamation = input.endsWith("!!!");
-        System.out.println("Заканчивается на '!!!': " + endsWithExclamation);
-
-        // 4. Проверить, начинается ли строка подстрокой "I like"
-        boolean startsWithILike = input.startsWith("I like");
-        System.out.println("Начинается с 'I like': " + startsWithILike);
-
-        // 5. Проверить, содержит ли строка подстроку "Java"
-        boolean containsJava = input.contains("Java");
-        System.out.println("Содержит 'Java': " + containsJava);
-
-        // 6. Найти позицию подстроки "Java"
-        int positionOfJava = input.indexOf("Java");
-        System.out.println("Позиция 'Java': " + positionOfJava);
-
-        // 7. Заменить все символы "а" на "о"
-        String replacedString = input.replace('а', 'o');
-        System.out.println("После замены 'а' на 'о': " + replacedString);
-
-        // 8. Преобразовать строку к верхнему регистру
-        String upperCaseString = input.toUpperCase();
-        System.out.println("Верхний регистр: " + upperCaseString);
-
-        // 9. Преобразовать строку к нижнему регистру
-        String lowerCaseString = input.toLowerCase();
-        System.out.println("Нижний регистр: " + lowerCaseString);
-
-        // 10. Вырезать строку "Java" с помощью метода substring
-        String extractedJava = input.substring(positionOfJava, positionOfJava + "Java".length());
-        System.out.println("Вырезанная строка 'Java': " + extractedJava);
     }
 }
