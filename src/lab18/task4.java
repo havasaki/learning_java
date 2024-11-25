2bca8eb583e87c00150e842f531951037e118d0a
havasaki <elcov.danilka@mail.ru>
2024-11-25 20:16:40 +0300

    create 4 tasks in lab18

---
+++ src/lab18/task4.java
@@ -0,0 +1,29 @@
+package lab18;
+
+import java.util.Scanner;
+
+public class task4 {
+    public void exceptionDemo() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        String intString = myScanner.next();
+
+        try {
+            int i = Integer.parseInt(intString);
+            System.out.println(2 / i);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input: not an integer.");
+        } catch (ArithmeticException e) {
+            System.out.println("Attempted division by zero.");
+        } finally {
+            myScanner.close(); // Закрываем сканер в любом случае
+            System.out.println("Scanner closed.");
+        }
+    }
+
+    public static void main(String[] args) {
+        task2 ex = new task2();
+        ex.exceptionDemo();
+    }
+}
+
