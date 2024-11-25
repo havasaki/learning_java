d0ccd8ef93d0c9d72a214bd8a619349c40289845
havasaki <elcov.danilka@mail.ru>
2024-11-25 18:37:37 +0300

    fix task5 in lab13

--- src/lab13/task5.java
+++ src/lab13/task5.java
@@ -1,53 +1,50 @@
 package lab13;
+class Number {
+    String codeCountry;
+    String firstTriple;
+    String secondTriple;
+    String lastFour;
+    String number;
+    StringBuilder temp;
 
+    private void formater() {
+        number = String.format("+%s(%s)-%s-%s", codeCountry,
+                firstTriple, secondTriple, lastFour);
+    }
+
+    public Number(String newNumber){
+
+        temp = new StringBuilder(newNumber.substring(newNumber.indexOf("+")+1));
+
+        lastFour = temp.substring(temp.length()-4);
+        secondTriple = temp.substring(temp.length()-7, temp.length()-4);
+        firstTriple = temp.substring(temp.length()-10, temp.length()-7);
+        codeCountry = temp.substring(0, temp.length()-10);
+
+        if (codeCountry.equals("8"))
+            codeCountry = "7";
+
+        formater();
+    }
+
+    @Override
+    public String toString() {
+        return "Number{" +
+                "number='" + number + '\'' +
+                '}';
+    }
+}
 public class task5 {
-    private String countryCode;
-    private String formattedNumber;
-
-    // Конструктор, принимающий строку с номером телефона
-    public task5(String phoneNumber) {
-        if (isValidPhoneNumber(phoneNumber)) {
-            parsePhoneNumber(phoneNumber);
-        } else {
-            throw new IllegalArgumentException("Invalid phone number format");
-        }
-    }
-
-    // Метод для проверки формата телефонного номера
-    private boolean isValidPhoneNumber(String phoneNumber) {
-        // Проверка на формат +<Код страны><10 цифр> или 8<10 цифр>
-        return phoneNumber.matches("\\+\\d{1,3}\\d{10}") || phoneNumber.matches("8\\d{10}");
-    }
-
-    // Метод для парсинга телефонного номера
-    private void parsePhoneNumber(String phoneNumber) {
-        if (phoneNumber.startsWith("+")) {
-            int len = phoneNumber.length();
-            countryCode = phoneNumber.substring(1, len - 10);
-            formattedNumber = phoneNumber.substring(len - 10);
-        } else if (phoneNumber.startsWith("8")) {
-            countryCode = "7"; // Код России без 8
-            formattedNumber = phoneNumber.substring(1);
-        }
-    }
-
-    // Метод для получения отформатированного номера
-    public String getFormattedNumber() {
-        if (formattedNumber != null) {
-            return "+" + countryCode + formattedNumber.substring(0, 3) + "-" +
-                    formattedNumber.substring(3, 6) + "-" + formattedNumber.substring(6);
-        }
-        return null;
-    }
-
     public static void main(String[] args) {
-        task5 phone1 = new task5("+79175655655");
-        task5 phone2 = new task5("89175655655");
-        task5 phone3 = new task5("+104289652211");
+        Number n1 = new Number("+79501829266");
+        Number n2 = new Number("89501829266");
+        Number n3 = new Number("+189501829266");
+        Number n4 = new Number("379501829266");
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
 
-        System.out.println(phone1.getFormattedNumber()); // +7 917-565-5655
-        System.out.println(phone2.getFormattedNumber()); // +7 917-565-5655
-        System.out.println(phone3.getFormattedNumber()); // +104 289-652-211
 
     }
 }
